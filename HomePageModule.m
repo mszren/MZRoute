3684e10715d8df6866c6ccef0695f57f9181b4c7
binbinShen <>
2018-06-13 18:58:34 +0800

    ss

--- HomePageModule.m
+++ HomePageModule.m
@@ -7,19 +7,19 @@
 //
 
 #import "HomePageModule.h"
-#import <MGJRouter/MGJRouter.h>
-#import "HomePageViewController.h"
+//#import <MGJRouter/MGJRouter.h>
+//#import "HomePageViewController.h"
 
 @implementation HomePageModule
 
-// 在load方法中自动注册，在主工程中不用写任何代码。
-+ (void)load {
-    [MGJRouter registerURLPattern:@"CTB://HomePage/PushMainVC" toHandler:^(NSDictionary *routerParameters) {
-        UINavigationController *navigationVC = routerParameters[MGJRouterParameterUserInfo][@"navigationVC"];
-        
-        HomePageViewController *homePageVC = [[HomePageViewController alloc] init];
-        [navigationVC pushViewController:homePageVC animated:YES];
-    }];
-}
+//// 在load方法中自动注册，在主工程中不用写任何代码。
+//+ (void)load {
+//    [MGJRouter registerURLPattern:@"CTB://HomePage/PushMainVC" toHandler:^(NSDictionary *routerParameters) {
+//        UINavigationController *navigationVC = routerParameters[MGJRouterParameterUserInfo][@"navigationVC"];
+//
+//        HomePageViewController *homePageVC = [[HomePageViewController alloc] init];
+//        [navigationVC pushViewController:homePageVC animated:YES];
+//    }];
+//}
 
 @end
